f9ba496227f9bc8fb3b684857b16e3d468713a74
gimama95 <mazzoni.giovannimarco@gmail.com>
2025-12-06 21:33:19 +0100

    Create data.js

---
+++ scripts/data.js
@@ -0,0 +1,13 @@
+// Lista degli audio da visualizzare
+const audioData = [
+    {
+        name: "Esempio 1",
+        file: "audio/esempio1.mp3",
+        thumb: "imgs/thumb1.jpg"
+    },
+    {
+        name: "Esempio 2",
+        file: "audio/esempio2.mp3",
+        thumb: "imgs/thumb2.jpg"
+    }
+];
